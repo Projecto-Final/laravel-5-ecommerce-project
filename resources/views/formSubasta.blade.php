85ee7ffd2ad5d12ddc7d8af0c0d2224614141838
dakota14 <nirk.maroto@gmail.com>
2015-05-08 16:54:25 +0200

    Comit URL

---
+++ resources/views/formSubasta.blade.php
@@ -0,0 +1,16 @@
+<?php use App\Articulo; ?>
+
+
+@extends('layouts.madre')
+@section('titulo', 'PAGINA PRINCIPAL')
+@stop
+@section('info_extra')
+@stop
+
+
+
+
+@section('articulos_subastas')
+@stop
+@section('content')
+@stop
