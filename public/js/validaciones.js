1a31ae4877f5611c41bd705adc41b897ab7f292e
wellspirit <adria.pozo.93@gmail.com>
2015-05-11 20:08:44 +0200

    Javascrip

---
+++ public/js/validaciones.js
@@ -0,0 +1,141 @@
+
+
+ /*window.onload = function() {
+
+
+	alert("funco");
+
+
+ }
+ */
+ var error = false;
+ function formValidator(){
+
+ 	error=false; 	
+ 	var formulario = document.getElementById('form-validate');
+ 	var porTagName = formulario.getElementsByTagName("input");
+ 	var current;
+ 	var val;
+ 	var aux;
+
+ /*	for (var i = 0; i < porTagName.length; i++) {
+ 		if(porTagName[i].type!="hidden"){
+ 			alert(porTagName[i].value);
+ 		}
+ 	}*/
+
+ 	for (i=0;i<porTagName.length;i++)
+ 	{
+ 		current = porTagName[i];
+ 		//alert(current.name);
+ 		if(current.type!="hidden"){
+ 			if(current.type!="button"){
+ 				if(current.type=="checkbox"){
+ 					//alert(current.name);
+ 					//alert(current.value);
+ 					//alert(current.checked);
+
+ 					if(current.checked==false){
+ 						alert("Deve aceptar las condiciones de uso y politica de privacidad");
+ 						getIdMsg(current,true,false);
+ 						//error=true;
+ 					}
+ 				}
+ 				val = current.value;
+
+ 				if(val!=""){
+	//el no campo esta vacio
+	getIdMsg(current,false,false);
+	if(current.name=="password"){
+
+
+		if(val.length<6){
+			// alert("value.leng"+val.length);
+			getIdMsg(current,true,true);
+		}else{
+			getIdMsg(current,false,true);
+		}
+	}
+	if(current.name=="password_confirmation"){
+		comprovarPass(current);
+		//var aux = comprovarPass(current);
+		//if(aux==false){
+			//error=true;	
+	//	}
+
+}
+
+}else{
+					//el campo esta vacio
+					getIdMsg(current,true,false);
+					//error=true;	
+				}
+
+			}
+		}
+	}
+	if(error==false){
+		//alert("submit");
+		formulario.submit();
+	}else{
+		//alert("no submit")
+		return false;
+	}
+}
+
+
+function getIdMsg(elem, si,especial){
+	var ele = elem.getAttribute("id");
+
+	if (ele == undefined) {
+		ele = elem.getAttribute("name")
+	};
+	// ERROR ESPECIAL O NO
+	if(especial==true){
+		ele += "_error2";
+	}else if(especial==false){
+		ele += "_error";
+	}
+
+	//  MOSTRAR O OCULTAR
+	if(si==true){
+		mostraError(ele);
+		
+	}else if(si==false){
+		ocultaError(ele);
+		
+	}
+		//return ele;
+	}
+	
+
+	function comprovarPass(element){
+		var pass1 = document.getElementById('password').value;
+		var pass2 = element.value;
+		if(pass1==pass2){
+		//si son iguales devuelve true
+		getIdMsg(element, false,true);
+		return true;
+	}else{
+		//al ser diferentes devuelve false
+		getIdMsg(element, true,true);
+		return false;
+	}
+}
+
+function validarEmail( email ) {
+	expr = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+	if ( !expr.test(email) ){
+       // alert("Error: La dirección de correo " + email + " es incorrecta.");
+       return false;
+   }
+}
+
+function mostraError(idElem) {
+	error=true;
+	document.getElementById(idElem).style.display="inline";
+}
+
+function ocultaError(idElem) {
+	document.getElementById(idElem).style.display="none";
+}
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,6 +2,8 @@
 
 @section('registro_login')
 
+<script type="text/javascript" src="{{ URL::asset('js/validaciones.js') }}"></script>
+
 <div id="sns_content" class="wrap layout-m">
 	<div class="container">
 		<div class="row">
@@ -35,12 +37,14 @@
 												<label for="nombre" class="required"><em>*</em>Nombre</label>
 												<div class="input-box">
 													<input type="text" id="nombre" name="nombre" value="{{ old('nombre') }}" title="Nombre" maxlength="255" class="input-text required-entry">
+													<span class='error' id='nombre_error'>&nbsp;Campo obligatorio</span></td>
 												</div>
 											</div>
 											<div class="field name-lastname">
 												<label for="apellidos" class="required"><em>*</em>Apellidos</label>
 												<div class="input-box">
 													<input type="text" id="apellido" name="apellido" value="{{ old('apellido') }}" title="Apellido" maxlength="255" class="input-text required-entry">
+													<span class='error' id='apellido_error'>&nbsp;Campo obligatorio</span></td>
 												</div>
 											</div>
 										</div>
@@ -49,13 +53,15 @@
 										<label for="apodo" class="required"><em>*</em>Apodo</label>
 										<div class="input-box">
 											<input type="text" id="username" class="input-text validate-email required-entry"  name="username" value="{{ old('username') }}">
+											<span class='error' id='username_error'>&nbsp;Campo obligatorio</span></td>
 										</div>
 									</li>
 									<li class="control">
 										<div class="input-box">
 											<input type="checkbox" name="politica_privacidad" title="Sign Up for Newsletter" value="1" id="politica_privacidad" class="checkbox">
 										</div>
-										<label for="politica_privacidad">Acepto las condiciones de uso y politica de privacidad.</label>
+										<label for="politica_privacidad">Acepto las condiciones de uso y politica de privacidad.</label></td>
+										<span class='error' id='politica_privacidad_error'>&nbsp;Debe aceptar las condiciones de uso y politica de privacidad</span></td>
 									</li>
 								</ul>
 							</div>
@@ -67,6 +73,8 @@
 											<label for="confirmation" class="required"><em>*</em>Email</label>
 											<div class="input-box">
 												<input type="email" id="email_address" name="email" value="{{ old('email') }}" title="Correo Electronico" class="input-text validate-email required-entry">
+												<span class='error' id='email_address_error'>&nbsp;Campo obligatorio</span></td>
+												<span class='error' id='email_address_error2'>&nbsp;Debe ser una direccion de correo valida</span></td>
 											</div>
 										</div>
 									</li>
@@ -75,12 +83,16 @@
 											<label for="password" class="required"><em>*</em>Contraseña</label>
 											<div class="input-box">
 												<input type="password" name="password" id="password" title="password" class="input-text required-entry validate-password">
+												<span class='error' id='password_error'>&nbsp;Campo obligatorio</span></td>
+												<span class='error' id='password_error2'>&nbsp;La contraseña debe se der de almenos 6 caracteres</span></td>
 											</div>
 										</div>
 										<div class="field">
 											<label for="confirmacion" class="required"><em>*</em>Confirmar Contraseña</label>
 											<div class="input-box">
-												<input type="password" name="password_confirmation" title="Confirm Password" class="input-text required-entry validate-cpassword">
+												<input type="password" id="password_confirmation" name="password_confirmation" title="Confirm Password" class="input-text required-entry validate-cpassword">
+												<span class='error' id='password_confirmation_error2'>&nbsp;Las contraseñas deben coincidir</span>
+												<span class='error' id='password_confirmation_error'>&nbsp;Campo obligatorio</span></td>
 											</div>
 										</div>
 									</li>
@@ -89,8 +101,10 @@
 							<div class="buttons-set">
 								<p class="required">* Campos requeridos</p>
 								<p class="back-link"><a href="index.html" class="back-link">« Regresar al inicio</a></p>
-								<button type="submit" title="Submit" class="button"><span><span>Registrar</span></span></button>
-							</div>
+
+								<!-- <button type="submit" title="Submit" class="button" onclick='formValidatorr()'><span><span>Registrar</span></span></button> -->
+								
+								<input type='button' title="Submit" class="button" onclick='formValidator()' value="Registrar"><span>
 						</form>
 					</div>
 				</div>
