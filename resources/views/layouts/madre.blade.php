0c77f47c1601de08b2f4d6cffa9ff0884c9240f6
TooPurpleForMe <sergio.sliver.93@gmail.com>
2015-05-13 15:53:30 +0200

    estrellitas en el usuario, le falta css y canviar los titulos,
    composer udate al descargar o composer install si esta de nuevo

--- resources/views/layouts/madre.blade.php
+++ resources/views/layouts/madre.blade.php
@@ -11,6 +11,10 @@
 	<!-- Bootstrap -->
 	<link rel="stylesheet" href="{{ url('css/font-awesome.min.css') }}">
 	<link href="{{ url('css/bootstrap.min.css') }}" rel="stylesheet">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+	<link href="{{url('../vendor/kartik-v/bootstrap-star-rating/css/star-rating.min.css')}}" media="all" rel="stylesheet" type="text/css" />
+	<script src="{{url('../vendor/kartik-v/bootstrap-star-rating/js/star-rating.min.js')}}" type="text/javascript"></script>
+	<link href="{{ url('css/bootstrap.min.css') }}" rel="stylesheet">
 	<link href="{{ url('css/main.css') }}" rel="stylesheet">
 	<link href="{{ url('css/perfil_user.css') }}" rel="stylesheet">
 	<link href="{{ url('css/subasta.css') }}" rel="stylesheet">
@@ -22,7 +26,6 @@
 		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
 		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 		<![endif]-->
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
 		<link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700" rel="stylesheet" type="text/css">
 		<link href="http://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" type="text/css">
 		<link href="http://fonts.googleapis.com/css?family=Roboto:400,100,300,500,700,900" rel="stylesheet" type="text/css">
