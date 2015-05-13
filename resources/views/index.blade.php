0637ed1327be5559bf16b564922ce97d4da038a7
erojutsu <btmcant92@gmail.com>
2015-05-14 00:46:14 +0200

    Laravel 5 index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,4 +1,4 @@
-<?php use App\Articulo; ?>
+<?php use App\Articulo; use App\Imagen; ?>
 
 
 @extends('layouts.madre')
@@ -87,19 +87,18 @@
 // $p = new Articulo;
 // Esperando implementacion de articulos.
 $articulos = Articulo::all();
-$temp = [["Caca de Perro",2],["Iphone 6",1299],["Smartphone 2",699],["Tablet 4",958],["HentaiKey",6969],["Manga 2",9999]];
-
+$imagenes = Imagen::where('articulo_id', '=', 31)->firstOrFail();
 ?>
-
 @forelse($articulos as $articulo)
 <!-- MUESTRA -->
+
                         <div class="item item-animate col-xs-6 col-sm-4 col-md-4 col-lg-2 col-phone-12" style="-webkit-animation-delay:0ms;-moz-animation-delay:0ms;-o-animation-delay:0ms;animation-delay:0ms;">
                           <div class="item-inner clearfix">
                             <div class="badges"> </div>
-                            <div class="item-img"> <a class="product-image" href="" title=" Retis lapen casen "> <span class="image-main"> <img src="" alt=" Retis lapen casen "> </span> </a> </div>
+                            <div class="item-img"> <a class="product-image" href="{{ url('subasta/'.$articulo['id']) }}" title=" Retis lapen casen "> <span class="image-main"> <img src="{{ url('images/subastas/'.$imagenes->imagen) }}" alt=" Retis lapen casen "> </span> </a> </div>
                             <div class="item-info">
                               <div class="info-inner">
-                                <div class="item-title"> <a href="http://demo.snstheme.com/sns-riveshop/index.php/retis-lapen.html" onclick="javascript: return true" title=" Retis lapen casen ">{{ $articulo['nombre_producto'] }} </a> </div>
+                                <div class="item-title"> <a href="" onclick="javascript: return true" title=" Retis lapen casen ">{{ $articulo['nombre_producto'] }} </a> </div>
                                 <div class="item-content clearfix">
                                   <div class="item-price">
                                     <div class="price-box"> <span class="price">{{ $articulo['puja_mayor'] }} <i class="fa fa-eur"></i></span> </div>
