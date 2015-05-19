921d6da02a5f0b9978ff92f74f4f182daf256637
erojutsu <btmcant92@gmail.com>
2015-05-19 18:27:33 +0200

    Temporal fix, index.

--- app/Http/Controllers/GlobalController.php
+++ app/Http/Controllers/GlobalController.php
@@ -8,79 +8,90 @@ use App\Imagen;
 
 class GlobalController extends Controller {
 
-    /*
-    * GLOBAL
-    */
-    public function __construct()
-    {
-    }
+	/*
+	* GLOBAL
+	*/
+	public function __construct()
+	{
+	}
 
 
-    /**
-     * 
-     *
-     * @return Response
-     */
-    public function get_allCategories()
-    {
-        return $data = Categoria::all();
-    }
+	/**
+	 * 
+	 *
+	 * @return Response
+	 */
+	public function get_allCategories()
+	{
+		return $data = Categoria::all();
+	}
 
-     /**
-     * Envia la info al index, y lo muestra.
-     *
-     * @return Response
-     */
-        public function index()
-        {
-            // Buscamos todos los Articulos  que tenemos ( si tenemos )
-            $Articulos = Articulo::all();
-             return view("index", ["subastas" => $Articulos] );
-        }
+	 /**
+	 * Envia la info al index, y lo muestra.
+	 *
+	 * @return Response
+	 */
+	 public function index()
+	 {
+		// Buscamos todos los Articulos  que tenemos ( si tenemos )
+	 	$arrayArtImg = [];
+	 	$Articulos = Articulo::all();
+	 	echo "<pre>";
+	 	foreach ($Articulos as $key => $Articulo) {
+	 		$imagenes =  $Articulo->imagenes;
+	 		foreach ($imagenes as $key => $imagen) {
+	 			if($key==0){
+	 				$arrayArtImg[$key] = [$Articulo, $imagen['imagen']];
+	 			}
+	 		}
+	 		echo "</pre>";
+	 	}
+	 	return view("index", ["subastas" =>$arrayArtImg] );
+	 }
 
 
 
-    /**
-    * 
-    * @return Response
-    */
-    public function get_selectedSubasta($idArticulo)
-    {
-        $articulo = Articulo::find($idArticulo);
+	/**
+	* 
+	* @return Response
+	*/
+	public function get_selectedSubasta($idArticulo)
+	{
+		$articulo = Articulo::find($idArticulo);
 
-        $aux = $articulo->pujas;
-        $pujas=count($aux);
+		$aux = $articulo->pujas;
+		$pujas=count($aux);
 
    /*     for ($i=0; $i < count($pujas[0]); $i++) {
-            $pujas[1][$i] = $pujas[$i]->usuario;
-        }*/
-        //mostrar las 3 ultimas pujas por el articulo
+			$pujas[1][$i] = $pujas[$i]->usuario;
+		}*/
+		//mostrar las 3 ultimas pujas por el articulo
 
 
-        $subastador = Usuario::find($articulo['subastador_id']);
-        $imagenes = $articulo->imagenes;
+		$subastador = Usuario::find($articulo['subastador_id']);
+		$imagenes = $articulo->imagenes;
 
-        return view("view_subasta", ["subasta" => $articulo , "subastador" => $subastador, "imagenes" => $imagenes, "pujas"=> $pujas] );
-    }
+		return view("view_subasta", ["subasta" => $articulo , "subastador" => $subastador, "imagenes" => $imagenes, "pujas"=> $pujas] );
+	}
 
-    public function buscar_subastas()
-    {
-        $urlParams=Input::all();
-        $buscar = $urlParams['buscar'];
-        echo "<h1>".$buscar."</h1>";
-        $categoria = $urlParams['categoria'];
-        echo "<pre>";
-        $subcat = Categoria::find($categoria)->subcategorias;
-        foreach ($subcat as $key => $scategoria) {
-            //echo $scategoria['id'];
-            $arts = Articulo::where('subcategoria_id', '=', $scategoria['id'])
-            ->where('nombre_producto', 'LIKE', '%'.$buscar.'%')
-            ->get();
-            echo "<h1>RESULTADO BUSQUEDA - ".count($arts)."</h1>";
-            foreach ($arts as $key => $art) {
-                echo $art["nombre_producto"];
-            }
-            echo "</pre>";
-        }
-    }
+	public function buscar_subastas()
+	{
+		$urlParams=Input::all();
+		$buscar = $urlParams['buscar'];
+		echo "<h1>".$buscar."</h1>";
+		$categoria = $urlParams['categoria'];
+		echo "<pre>";
+		$subcat = Categoria::find($categoria)->subcategorias;
+		foreach ($subcat as $key => $scategoria) {
+			//echo $scategoria['id'];
+			$arts = Articulo::where('subcategoria_id', '=', $scategoria['id'])
+			->where('nombre_producto', 'LIKE', '%'.$buscar.'%')
+			->get();
+			echo "<h1>RESULTADO BUSQUEDA - ".count($arts)."</h1>";
+			foreach ($arts as $key => $art) {
+				echo $art["nombre_producto"];
+			}
+			echo "</pre>";
+		}
+	}
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -12,131 +12,131 @@
 
 
 <div class="contenedor-informacion-clientes">
-    <div class="container">
-      <div class="row">
-        <div class="col-sm-3 col-xs-6 col-phone-12 block-layout">
-          <div class="item">
-            <div class="icon pull-left"> <i class="fa fa-truck">&nbsp;</i> </div>
-            <div class="info">
-              <h5>Envio gratuito</h5>
-              <p>En compras mayores 100€</p>
-            </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-3 col-xs-6 col-phone-12 block-layout">
+        <div class="item">
+          <div class="icon pull-left"> <i class="fa fa-truck">&nbsp;</i> </div>
+          <div class="info">
+            <h5>Envio gratuito</h5>
+            <p>En compras mayores 100€</p>
           </div>
         </div>
-        <div class="col-sm-3 col-xs-6 col-phone-12 block-layout">
-          <div class="item">
-            <div class="icon pull-left"> <i class="fa fa-history">&nbsp;</i> </div>
-            <div class="info">
-              <h5>Devolucion Gratuita</h5>
-              <p>30 dias garantia de devolucion.</p>
-            </div>
+      </div>
+      <div class="col-sm-3 col-xs-6 col-phone-12 block-layout">
+        <div class="item">
+          <div class="icon pull-left"> <i class="fa fa-history">&nbsp;</i> </div>
+          <div class="info">
+            <h5>Devolucion Gratuita</h5>
+            <p>30 dias garantia de devolucion.</p>
           </div>
         </div>
-        <div class="col-sm-3 col-xs-6 col-phone-12 block-layout">
-          <div class="item">
-            <div class="icon pull-left"> <i class="fa fa-money">&nbsp;</i> </div>
-            <div class="info">
-              <h5>Calidad premium</h5>
-              <p>Las mejores pujas de la web</p>
-            </div>
+      </div>
+      <div class="col-sm-3 col-xs-6 col-phone-12 block-layout">
+        <div class="item">
+          <div class="icon pull-left"> <i class="fa fa-money">&nbsp;</i> </div>
+          <div class="info">
+            <h5>Calidad premium</h5>
+            <p>Las mejores pujas de la web</p>
           </div>
         </div>
-        <div class="col-sm-3 col-xs-6 col-phone-12 block-layout">
-          <div class="item">
-            <div class="icon pull-left"> <i class="fa fa-gift">&nbsp;</i></div>
-            <div class="info">
-              <h5>Atencion al cliente</h5>
-              <p>Lun a Vie de 7.30 a 22.00</p>
-            </div>
+      </div>
+      <div class="col-sm-3 col-xs-6 col-phone-12 block-layout">
+        <div class="item">
+          <div class="icon pull-left"> <i class="fa fa-gift">&nbsp;</i></div>
+          <div class="info">
+            <h5>Atencion al cliente</h5>
+            <p>Lun a Vie de 7.30 a 22.00</p>
           </div>
         </div>
       </div>
     </div>
   </div>
+</div>
 @stop
 
 
 @section('categorias_i_sponsor')
-  @include('get_categorias')
+@include('get_categorias')
 @stop
 
 
 @section('articulos_subastas')
 <div id="sns_content" class="wrap layout-m">
-    <div class="container">
-      <div class="row">
-        <div id="sns_main" class="col-md-12 col-main">
-          <div id="sns_mainmidle">
-            
-            <div id="sns_producttabs3375621631430404908" class="sns-producttabs">
-              <div class="sns-pdt-head">
-                <h2>NUEVAS SUBASTAS</h2>
-              </div>
-              <div class="sns-pdt-container"> 
-                <!--Begin Tab Nav --> 
-                
-                <!-- End Tab Nav --> 
-                <!--Begin Tab Content -->
-                <div class="sns-pdt-content wide-4 normal-4 tabletlandscape-3 tabletportrait-3 mobilelandscape-2 mobileportrait-1">
-                  <div class="pdt-content tab-content-actived is-loaded pdt_39">
-                    <div class="pdt-list products-grid zoomOut play">
-                      <div class="inner-3267714721430404908"> 
-                        <!-- ARTICULOS --> 
+  <div class="container">
+    <div class="row">
+      <div id="sns_main" class="col-md-12 col-main">
+        <div id="sns_mainmidle">
 
-<?php
+          <div id="sns_producttabs3375621631430404908" class="sns-producttabs">
+            <div class="sns-pdt-head">
+              <h2>NUEVAS SUBASTAS</h2>
+            </div>
+            <div class="sns-pdt-container"> 
+              <!--Begin Tab Nav --> 
+
+              <!-- End Tab Nav --> 
+              <!--Begin Tab Content -->
+              <div class="sns-pdt-content wide-4 normal-4 tabletlandscape-3 tabletportrait-3 mobilelandscape-2 mobileportrait-1">
+                <div class="pdt-content tab-content-actived is-loaded pdt_39">
+                  <div class="pdt-list products-grid zoomOut play">
+                    <div class="inner-3267714721430404908"> 
+                      <!-- ARTICULOS --> 
+
+                      <?php
 // $p = new Articulo;
 // Esperando implementacion de articulos.
 // como funcionan
 // $img = Articulo::find(3)->imagenes;
-?>
-@forelse($subastas as $subasta)
-<!-- MUESTRA -->
+                      ?>
+                      @forelse($subastas as $subasta)
 
-                        <div class="item item-animate col-xs-12 col-sm-4 col-md-4 col-lg-2" style="-webkit-animation-delay:0ms;-moz-animation-delay:0ms;-o-animation-delay:0ms;animation-delay:0ms;">
-                          <div class="item-inner clearfix">
-                            <div class="badges"> </div>
-                            <div class="item-img"> <a class="product-image" href="{{ url('subasta/'.$subasta['id']) }}" title=" Retis lapen casen "> <span class="image-main"> <img src="{{ url('images/subastas/') }}" alt=" Retis lapen casen "> </span> </a> </div>
-                            <div class="item-info">
-                              <div class="info-inner">
-                                <div class="item-title"> <a href="" onclick="javascript: return true" title=" Retis lapen casen ">{{ $subasta['nombre_producto'] }} </a> </div>
-                                <div class="item-content clearfix">
-                                  <div class="item-price">
-                                    <div class="price-box"> <span class="price">{{ $subasta['puja_mayor'] }} <i class="fa fa-eur"></i></span> </div>
-                                  </div>
+                      <!-- MUESTRA -->
+                      <div class="item item-animate col-xs-12 col-sm-4 col-md-4 col-lg-2" style="-webkit-animation-delay:0ms;-moz-animation-delay:0ms;-o-animation-delay:0ms;animation-delay:0ms;">
+                        <div class="item-inner clearfix">
+                          <div class="badges"> </div>
+                          <div class="item-img"> <a class="product-image" href="{{ url('subasta/'.$subasta[0]['id']) }}" title=" Retis lapen casen "> <span class="image-main"> <img src="{{ url('images/subastas/'.$subasta[1]) }}" alt=" Retis lapen casen "> </span> </a> </div>
+                          <div class="item-info">
+                            <div class="info-inner">
+                              <div class="item-title"> <a href="" onclick="javascript: return true" title=" Retis lapen casen ">{{ $subasta[0]['nombre_producto'] }} </a> </div>
+                              <div class="item-content clearfix">
+                                <div class="item-price">
+                                <div class="price-box"> <span class="price">{{ $subasta[0]['puja_mayor'] }} <i class="fa fa-eur"></i></span> </div>
                                 </div>
-                                <div class="rating">
-                                  <p class="no-rating"><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i></p>
-                                </div>
-                                <button title="Add to Cart" class="btn-cart" onclick="">Subastar</button>
                               </div>
+                              <div class="rating">
+                                <p class="no-rating"><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i></p>
+                              </div>
+                              <button title="Add to Cart" class="btn-cart" onclick="">Subastar</button>
                             </div>
                           </div>
                         </div>
-                        <!-- FIN MUESTRA -->
-@empty
-    <p>No hay registros en la base de datos... a la espera...</p>
-@endforelse
-
- <!-- ARTICULOS -->
-                        <div class="clearfix visible-xs"></div>
-                        <div class="clearfix visible-md"></div>
-                        <div class="clearfix visible-sm"></div>
-                        <div class="clearfix visible-lg"></div>
                       </div>
+                      <!-- FIN MUESTRA -->
+                      @empty
+                      <p>No hay registros en la base de datos... a la espera...</p>
+                      @endforelse
+
+                      <!-- ARTICULOS -->
+                      <div class="clearfix visible-xs"></div>
+                      <div class="clearfix visible-md"></div>
+                      <div class="clearfix visible-sm"></div>
+                      <div class="clearfix visible-lg"></div>
                     </div>
-                    <div class="pdt-loadmore data-inserted" data-href="pdt_39" data-type="category" data-catid="39" data-orderby="position" data-all="20" data-start="6" data-loadnum="6">
-                      <div class="mas" style="display: block;"> <span class="loadmore button"><i class="fa fa-long-arrow-down"></i> VER MAS PRODUCTOS <i class="fa fa-long-arrow-down"></i></span></div>
-                    </div>
+                  </div>
+                  <div class="pdt-loadmore data-inserted" data-href="pdt_39" data-type="category" data-catid="39" data-orderby="position" data-all="20" data-start="6" data-loadnum="6">
+                    <div class="mas" style="display: block;"> <span class="loadmore button"><i class="fa fa-long-arrow-down"></i> VER MAS PRODUCTOS <i class="fa fa-long-arrow-down"></i></span></div>
                   </div>
                 </div>
               </div>
-              <!--End Tab Content --> 
             </div>
+            <!--End Tab Content --> 
           </div>
         </div>
       </div>
     </div>
   </div>
+</div>
 
 @stop
 @section('content')
