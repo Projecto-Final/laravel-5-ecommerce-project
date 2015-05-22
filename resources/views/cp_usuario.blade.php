e12e16cafd048325697b06fc72cffa9f6ee2e8a2
erojutsu <btmcant92@gmail.com>
2015-05-22 02:13:00 +0200

    Laravel 5 Usuario CP CSS++

--- resources/views/cp_usuario.blade.php
+++ resources/views/cp_usuario.blade.php
@@ -1,6 +1,3 @@
-<?php use App\Articulo; ?>
-<?php use App\Usuario; ?>
-
 @extends('layouts.madre')
 
 
@@ -14,24 +11,20 @@
 <div id="sns_content" class="wrap layout-m">
 
   <div class="container-fluid header_perfil" >
-    <div class="row back_img" style="background-image: url({{ url('images/profiles/'.$imagBack) }});">
-      <div id="" class="col-md-12 col-main col-xs-12" >
-        <div id="">
-          <div class="stadistic_info_user col-xs-12 col-md-12">
-           <div class="container">
-            <div class="img_perfil col-xs-12" style="background-image: url({{ url('images/profiles/'.$imagPerf) }}) ;"></div>
- 
+    <div class="row back_img" style="background-image: url({{ url('images/profiles_wallpapers/'.$imagBack) }});">
+      <div class="stadistic_info_user col-xs-12 col-md-12">
+       <div class="container">
+        <div class="img_perfil col-xs-12" style="background-image: url({{ url('images/profiles/'.$imagPerf) }}) ;"></div>
 
-            <div class="col-md-8 info_active col-xs-12">
-              Compras {{$ncompras}} <i class="fa fa-shopping-cart"></i>  || Ventas {{$nventas}} <i class="fa fa-money"></i>
-              || Pujas {{$npujas}} <i class="fa fa-fire"></i>
 
-            </br>
-            Reputacion
-            <input id="input-id" type="number" data-min="0" data-max="5" class="rating" data-show-caption="false" data-show-clear="false" data-disabled="true" data-size="xs" value="{{$reputacion}}"></input>
-          </div> 
-        </div>
-      </div>
+        <div class="col-md-8 info_active col-xs-12">
+          Compras {{$ncompras}} <i class="fa fa-shopping-cart"></i>  || Ventas {{$nventas}} <i class="fa fa-money"></i>
+          || Pujas {{$npujas}} <i class="fa fa-fire"></i>
+
+        </br>
+        Reputacion
+        <input id="input-id" type="number" data-min="0" data-max="5" class="rating" data-show-caption="false" data-show-clear="false" data-disabled="true" data-size="xs" value="{{$reputacion}}"></input>
+      </div> 
     </div>
   </div>
 </div>
@@ -46,7 +39,7 @@
          <p></p>
          <button class="bb" onclick="pujas();" >Mis Pujas</button>  
          <button class="bb" onclick="confPuj()" >Configuraciones Pujas</button>
-        <p></p>
+         <p></p>
          <button class="bb" onclick="subastas();" >Mis Subastas</button>
          <button class="bb" onclick="compras();" >Mis Compras</button>
          <button class="bb" onclick="ventas();" >Mis Ventas</button>
