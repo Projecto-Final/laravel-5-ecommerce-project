d38d3db6111c65a058e5f4a85f93c0e8e0d2df1b
wellspirit <adria.pozo.93@gmail.com>
2015-05-22 15:00:54 +0200

    mega chute

--- app/Http/Controllers/GlobalController.php
+++ app/Http/Controllers/GlobalController.php
@@ -105,9 +105,12 @@ if (Auth::check())//hay que añadir el ACTIVO
 	$logueado = true;
 	$user_id = Auth::user()->id;
 	$user = Usuario::find($user_id);
+}else{
+	$logueado = false;
 }
+
 //si el usuario es el propietario o el admin
-	if($user_id==$articulo['subastador_id'] || $user['permisos']==1){
+	
 
 
 		$aux = $articulo->pujas;
@@ -117,48 +120,15 @@ if (Auth::check())//hay que añadir el ACTIVO
 		$subcategoria = $articulo->subcategoria; 
 		$categoria = $subcategoria->categoria;
 
-		$nume=0;
+	
 
 
 		return response()->view("view_subasta", ["subasta" => $articulo , "subastador" => $subastador, "imagenes" => $imagenes, "pujas"=> $pujas, "subcategoria"=>$subcategoria, "categoria"=> $categoria, "logueado"=>$logueado]);
-		//->withInput()->with('message', Session::get('message'));
-//si eres un usuario logueado
-	}else{
-		$aux = $articulo->pujas;
-		$subastador = Usuario::find($articulo['subastador_id']);
-		$pujas = count($aux);
-		$imagenes = $articulo->imagenes;
-		$subcategoria = $articulo->subcategoria; 
-		$categoria = $subcategoria->categoria;
-
-		$nume=0;
-		//ultimas pujas y su usuario
-
-
-		return response()->view("view_subasta", ["subasta" => $articulo , "subastador" => $subastador, "imagenes" => $imagenes, "pujas"=> $pujas, "subcategoria"=>$subcategoria, "categoria"=> $categoria]);
-		//->withInput()->with('message', Session::get('message'));
-	}
-
-
-}else{
-	$aux = $articulo->pujas;
-	$subastador = Usuario::find($articulo['subastador_id']);
-	$pujas = count($aux);
-	$imagenes = $articulo->imagenes;
-	$subcategoria = $articulo->subcategoria; 
-	$categoria = $subcategoria->categoria;
-
-	$nume=0;
-		//ultimas pujas y su usuario
-
-	
-
-	return response()->view("view_subasta", ["subasta" => $articulo , "subastador" => $subastador, "imagenes" => $imagenes, "pujas"=> $pujas, "subcategoria"=>$subcategoria, "categoria"=> $categoria])
-	->withInput()->with('message', Session::get('message'));
+		
 
 }
 
-}
+
 
 
 
