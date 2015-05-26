0ed8cf0090187d6a3a568698e4e14cbe6058a75b
erojutsu <btmcant92@gmail.com>
2015-05-27 01:27:59 +0200

    Eliminar no se necesita

--- app/Http/Controllers/PrintController.php
+++
@@ -1,9 +0,0 @@
-<?php
-class PrintController extends \BaseController 
-{
-    public function index()
-    {
-        $pdf = PDF::loadHTML('<h1>Hello World!!</h1>');
-        return $pdf->stream();
-    }
-}
