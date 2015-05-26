32f7f017dec3177667006177f2c16e80c641fe95
Wellspirit <adria.pozo.93@gmail.com>
2015-05-27 01:38:56 +0200

    dfgsdg

--- database/seeds/Metodo_enviosTableSeeder.php
+++ database/seeds/Metodo_enviosTableSeeder.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
-use App\Subcategoria;
+use App\Metodo_envio;
 
 class Metodo_enviosTableSeeder extends Seeder {
 
@@ -9,10 +9,19 @@ class Metodo_enviosTableSeeder extends Seeder {
 	{
 		DB::table('Metodo_envios')->delete();
 
-		Metodo_pago::create([
-			'nombre' => 'Videojuegos',
-			'descripcion' => 'Encuentra todo tipo de juegos, a precios de escándalo.',
-			'categoria_id' => 1,
+		Metodo_envio::create([
+			'nombre' => 'Envío Express',
+			'descripcion' => 'Este método de envío es lo más rápido disponible. El tiempo de entrega es de 3 a 5 días laborables para todos los mayores destinos.',
+		]);
+
+		Metodo_envio::create([
+			'nombre' => 'Envío Estándar',
+			'descripcion' => 'El envío estándar tarda un poco más que el envío acelerado pero cuesta menos. El tiempo de entrega es de 6 a 8 días laborables para todos los mayores destinos.',
+		]);
+
+			Metodo_envio::create([
+			'nombre' => 'Envío Económico',
+			'descripcion' => 'El plazo de entrega es de 10 a 20 días laborables para los destinos principales. Esta forma de envío se caracteriza por sus gastos bajísimos a pesar de su plazo de entrega más largo.',
 		]);
 	}
 }
---
+++ database/seeds/Metodo_pagosTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Metodo_envio;
+
+class Metodo_pagosTableSeeder extends Seeder {
+
+	public function run()
+	{
+		DB::table('Metodo_pagos')->delete();
+
+		Metodo_pago::create([
+			'nombre' => 'PayPal',
+			'descripcion' => 'Paga con tu cuenta de PayPal como tu sabes.',
+		]);
+
+		Metodo_pago::create([
+			'nombre' => 'Cheques o giros postales',
+			'descripcion' => 'Paga con Cheques o giros postales.',
+		]);
+
+			Metodo_pago::create([
+			'nombre' => 'Pagos contra reembolso',
+			'descripcion' => 'Pagos contra reembolso en la mismísima entrega.',
+		]);
+	}
+}
