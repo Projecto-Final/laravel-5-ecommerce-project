6cfa0222692756c5a67d751138a97a8429040040
erojutsu <btmcant92@gmail.com>
2015-05-27 22:26:49 +0200

    fixdiv

--- resources/views/admin/editar_usuario.blade.php
+++ resources/views/admin/editar_usuario.blade.php
@@ -26,54 +26,54 @@
 					<input type="hidden" name="_token" value="{{{ csrf_token() }}}" />
 					<!-- text input -->
 					<h3 class="text-aqua">Detalles Usuario</h3>
-						<label>Imagen Perfil</label>
-						@if(!(isset($errors)&&($errors->first('imagen_perfil')!=null)))
-						<div class="form-group">
+					<label>Imagen Perfil</label>
+					@if(!(isset($errors)&&($errors->first('imagen_perfil')!=null)))
+					<div class="form-group">
 						<input type="text" name="imagen_perfil" class="form-control" value="{{ $usuario['imagen_perfil'] }}">
 						<p class="text-red">^-Falta añadir un cargador de imagenes! </p>
-						</div>
-						@else
-						<div class="form-group has-error">
-						<input type="text" name="imagen_perfil" class="form-control" value="{{ $usuario['imagen_perfil'] }}">
-						<p class="text-red">^-Falta añadir un cargador de imagenes! </p>
-							<i class="fa fa-times-circle-o"></i>
-							<label class="control-label" for="inputError">{{$errors->first('imagen_perfil')}}</label>
-						</div>
-						@endif
-						<label>Nombre Usuario ( Publico )</label>
-						@if(!(isset($errors)&&($errors->first('username')!=null)))
-						<div class="form-group">
-						<input type="text" name="username" class="form-control" value="{{ $usuario['username'] }}">
-						</div>
-						@else
-						<div class="form-group has-error">
-						<input type="text" name="username" class="form-control" value="{{ $usuario['username'] }}">
-							<i class="fa fa-times-circle-o"></i>
-							<label class="control-label" for="inputError">{{$errors->first('username')}}</label>
-						</div>
-						@endif
-						<label>Nombre</label>
-						@if(!(isset($errors)&&($errors->first('nombre')!=null)))
-						<div class="form-group">
-						<input type="text" name="nombre" class="form-control" value="{{ $usuario['nombre'] }}">
-						</div>
-						@else
-						<div class="form-group has-error">
-						<input type="text" name="nombre" class="form-control" value="{{ $usuario['nombre'] }}">
-							<i class="fa fa-times-circle-o"></i>
-							<label class="control-label" for="inputError">{{$errors->first('nombre')}}</label>
-						</div>
-						@endif
 					</div>
+					@else
+					<div class="form-group has-error">
+						<input type="text" name="imagen_perfil" class="form-control" value="{{ $usuario['imagen_perfil'] }}">
+						<p class="text-red">^-Falta añadir un cargador de imagenes! </p>
+						<i class="fa fa-times-circle-o"></i>
+						<label class="control-label" for="inputError">{{$errors->first('imagen_perfil')}}</label>
+					</div>
+					@endif
+					<label>Nombre Usuario ( Publico )</label>
+					@if(!(isset($errors)&&($errors->first('username')!=null)))
+					<div class="form-group">
+						<input type="text" name="username" class="form-control" value="{{ $usuario['username'] }}">
+					</div>
+					@else
+					<div class="form-group has-error">
+						<input type="text" name="username" class="form-control" value="{{ $usuario['username'] }}">
+						<i class="fa fa-times-circle-o"></i>
+						<label class="control-label" for="inputError">{{$errors->first('username')}}</label>
+					</div>
+					@endif
+					<label>Nombre</label>
+					@if(!(isset($errors)&&($errors->first('nombre')!=null)))
+					<div class="form-group">
+						<input type="text" name="nombre" class="form-control" value="{{ $usuario['nombre'] }}">
+					</div>
+					@else
+					<div class="form-group has-error">
+						<input type="text" name="nombre" class="form-control" value="{{ $usuario['nombre'] }}">
+						<i class="fa fa-times-circle-o"></i>
+						<label class="control-label" for="inputError">{{$errors->first('nombre')}}</label>
+					</div>
+					@endif
+
 					<div class="form-group">
 						<label>Apellido</label>
 						@if(!(isset($errors)&&($errors->first('apellido')!=null)))
 						<div class="form-group">
-						<input type="text" name="apellido" class="form-control" value="{{ $usuario['apellido'] }}">
+							<input type="text" name="apellido" class="form-control" value="{{ $usuario['apellido'] }}">
 						</div>
 						@else
 						<div class="form-group has-error">
-						<input type="text" name="apellido" class="form-control" value="{{ $usuario['apellido'] }}">
+							<input type="text" name="apellido" class="form-control" value="{{ $usuario['apellido'] }}">
 							<i class="fa fa-times-circle-o"></i>
 							<label class="control-label" for="inputError">{{$errors->first('apellido')}}</label>
 						</div>
@@ -83,11 +83,11 @@
 						<label>Email</label>
 						@if(!(isset($errors)&&($errors->first('email')!=null)))
 						<div class="form-group">
-						<input type="text" name="email" class="form-control" value="{{ $usuario['email'] }}">
+							<input type="text" name="email" class="form-control" value="{{ $usuario['email'] }}">
 						</div>
 						@else
 						<div class="form-group has-error">
-						<input type="text" name="email" class="form-control" value="{{ $usuario['email'] }}">
+							<input type="text" name="email" class="form-control" value="{{ $usuario['email'] }}">
 							<i class="fa fa-times-circle-o"></i>
 							<label class="control-label" for="inputError">{{$errors->first('email')}}</label>
 						</div>
@@ -97,50 +97,51 @@
 						<label>Reputación</label>
 						@if(!(isset($errors)&&($errors->first('reputacion')!=null)))
 						<div class="form-group">
-						<textarea class="form-control"  name="reputacion" rows="3" placeholder="Introduce reputacion...">{{ $usuario['reputacion'] }}</textarea>
+							<textarea class="form-control"  name="reputacion" rows="3" placeholder="Introduce reputacion...">{{ $usuario['reputacion'] }}</textarea>
 						</div>
 						@else
 						<div class="form-group has-error">
-						<textarea class="form-control"  name="reputacion" rows="3" placeholder="Introduce reputacion...">{{ $usuario['reputacion'] }}</textarea>
+							<textarea class="form-control"  name="reputacion" rows="3" placeholder="Introduce reputacion...">{{ $usuario['reputacion'] }}</textarea>
 							<i class="fa fa-times-circle-o"></i>
 							<label class="control-label" for="inputError">{{$errors->first('reputacion')}}</label>
 						</div>
 						@endif
 					</div>
 					<h3 class="text-aqua">Configuración Permisos</h3>
-						@if(!(isset($errors)&&($errors->first('precio_venta')!=null)))
+					@if(!(isset($errors)&&($errors->first('precio_venta')!=null)))
 					<div class="form-group">
-							<select name="permisos" class="form-control">
-								@if($usuario['permisos']==0)
-								<option value="0" selected>Usuario Normal</option>
-								<option value="1">Usuario Administrador</option>
-								@else
-								<option value="0">Usuario Normal</option>
-								<option value="1" selected>Usuario Administrador</option>
-								@endif
-							</select>
-						</div>
-						@else
-						<div class="form-group has-error">
-							<select name="permisos" class="form-control">
-								@if($usuario['permisos']==0)
-								<option value="0" selected>Usuario Normal</option>
-								<option value="1">Usuario Administrador</option>
-								@else
-								<option value="0">Usuario Normal</option>
-								<option value="1" selected>Usuario Administrador</option>
-								@endif
-							</select>
-							<i class="fa fa-times-circle-o"></i>
-							<label class="control-label" for="inputError">{{$errors->first('precio_venta')}}</label>
-						</div>
-						@endif
+						<select name="permisos" class="form-control">
+							@if($usuario['permisos']==0)
+							<option value="0" selected>Usuario Normal</option>
+							<option value="1">Usuario Administrador</option>
+							@else
+							<option value="0">Usuario Normal</option>
+							<option value="1" selected>Usuario Administrador</option>
+							@endif
+						</select>
 					</div>
-					<button class="btn btn-block btn-success btn-flat">Guardar <i class="fa fa-save"></i></button>
-				</form>
-			</div><!-- /.box-body -->
-		</div><!-- /.box -->
+					@else
+					<div class="form-group has-error">
+						<select name="permisos" class="form-control">
+							@if($usuario['permisos']==0)
+							<option value="0" selected>Usuario Normal</option>
+							<option value="1">Usuario Administrador</option>
+							@else
+							<option value="0">Usuario Normal</option>
+							<option value="1" selected>Usuario Administrador</option>
+							@endif
+						</select>
+						<i class="fa fa-times-circle-o"></i>
+						<label class="control-label" for="inputError">{{$errors->first('precio_venta')}}</label>
+					</div>
+					@endif
+				</div>
+				<button class="btn btn-block btn-success btn-flat">Guardar <i class="fa fa-save"></i></button>
+			</form>
+		</div><!-- /.box-body -->
 	</div>
+</div><!-- /.box -->
+</div>
 </div>
 @stop
 
