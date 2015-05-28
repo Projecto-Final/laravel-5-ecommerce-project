96261b3e06e3398ca402948cd8406c451b4b9ca3
erojutsu <btmcant92@gmail.com>
2015-05-28 14:15:07 +0200

    XML working

--- resources/views/admin/factura_xml.blade.php
+++ resources/views/admin/factura_xml.blade.php
@@ -1,11 +1,11 @@
-<?xml version="1.0" encoding="UTF-8"?>
+<?xml version="1.0" encoding="iso-8859-1"?>
 <factura>
   <id>{{ $factura['id']}}</id>
   <nombre>{{ $usuario['nombre'] }}</nombre>
   <direccion>{{ $usuario['direccion'] }}</direccion>
   <servicio>
     <nombre>Porroga</nombre>
-    <articulo>'{{--*/ echo toString($articulo['nombre_producto']); /*--}}</articulo>
+    <articulo>'{{--*/ echo $articulo['nombre_producto']; /*--}}</articulo>
     <coste>{{ $factura['cantidad_pagada'] }}</coste>
     <iva>{{--*/ echo round(($factura['cantidad_pagada']*0.21), 2); /*--}}</iva>
   </servicio>
