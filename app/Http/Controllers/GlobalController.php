8c1ebc6cdbfa3bc5f2590321b904a9452c6ef0cc
dakota14 <nirk.maroto@gmail.com>
2015-05-28 17:51:13 +0200

    chapuza

--- app/Http/Controllers/GlobalController.php
+++ app/Http/Controllers/GlobalController.php
@@ -322,6 +322,10 @@ class GlobalController extends Controller {
 				$user[1] = $user[0]->localidad;
 				return view('perfil',['user' => $user]);
 			}
+		}else{
+			$user[0] = Usuario::find($id);
+			$user[1] = $user[0]->localidad;
+			return view('perfil',['user' => $user]);
 		}		
 
 
