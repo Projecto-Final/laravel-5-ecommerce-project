f32be2b957850b457549b9e18a7861013a7fed06
Wellspirit <adria.pozo.93@gmail.com>
2015-05-28 02:55:28 +0200

    adfa

--- resources/views/nosotros.blade.php
+++ resources/views/nosotros.blade.php
@@ -8,6 +8,7 @@
 	<div class="row">
 		<div class="col-md-offset-4 col-xs-12 col-sm-12"><h1>Nosotros</h1></div>
 		<div class="col-xs-12 col-sm-12"><h5>Pàgina de : </h5><img class="empresa" src="{{ url('images/empresa.png') }}"/></div>
+		<div class="col-xs-12 col-sm-12"><img  src="{{ url('images/pro.jpg') }}"/></div>
 </div>
 </div>
 @stop
